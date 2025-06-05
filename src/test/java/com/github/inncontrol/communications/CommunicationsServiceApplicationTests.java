5ca0ccc884720a09f7773796052ae03ee6371add
Ryzeon <alex.ramon2004@gmail.com>
2025-06-04 20:48:10 -0500

    first commit

---
+++ src/test/java/com/github/inncontrol/communications/CommunicationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.inncontrol.communications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
